3751ef8d6b68eab6bd2577ea00cea13193e5ecf1
gilad111 <giladyativ10@gmail.com>
2024-04-23 17:00:39 +0300

    add Firebase login logic.

--- app/src/main/java/com/example/memorygame/LoginActivity.java
+++ app/src/main/java/com/example/memorygame/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.memorygame;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,18 +13,18 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -47,7 +49,7 @@ public class LoginActivity extends AppCompatActivity {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-                                auth = FirebaseAuth().getInstance();
+                                auth = FirebaseAuth.getInstance();
                                 Glide.with(LoginActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
                                 name.setText(auth.getCurrentUser().getDisplayName());
                                 mail.setText(auth.getCurrentUser().getEmail());
@@ -67,8 +69,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.login_activity);
 
+        FirebaseApp.initializeApp(this);
+        imageView = findViewById(R.id.profileImage);
+       name = findViewById(R.id.nameTV);
+        mail = findViewById(R.id.mailTV);
 
-    });
+        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.client_id)).requestEmail().build() ;
+        googleSignInClient = GoogleSignIn.getClient(LoginActivity.this,options);
+        auth = FirebaseAuth.getInstance() ;
+       SignInButton signInButton = findViewById(R.id.signIn) ;
+       signInButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+              Intent intent = googleSignInClient.getSignInIntent() ;
+              activityResultLauncher.launch(intent) ;
+           }
+       });
+    }
 }
