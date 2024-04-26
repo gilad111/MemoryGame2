8ec8c61e847b4c551d98aa6985a1ac75ce0871fd
gilad111 <giladyativ10@gmail.com>
2024-04-26 16:41:55 +0300

    Add of 2 variables (lastOpenedCard1,lastOpenedCard2) that they are the 2 cards that open in the last turn in the knock function. In the end of the reset function this variables we'll go back to be -1 their first value.

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -1,20 +1,21 @@
 package com.example.memorygame;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +25,15 @@ public class MainActivity extends AppCompatActivity {
     int countPlayer1 = 0;
     int countPlayer2 = 0;
     int card1;
+    int lastOpenedCard1 = -1;
+    int lastOpenedCard2 = -1;
     String turn = "counterPlayer1";
     Integer[] drawableIds = {
             // Array of drawable resource IDs
             R.drawable.num1, R.drawable.num2, R.drawable.num3, R.drawable.num4, R.drawable.num5, R.drawable.num6, R.drawable.num7, R.drawable.num8,
             R.drawable.num1, R.drawable.num2, R.drawable.num3, R.drawable.num4, R.drawable.num5, R.drawable.num6, R.drawable.num7, R.drawable.num8
     };
+
     int[] viewId = {
             //creates arr of view Ids
             R.id.im_11, R.id.im_12, R.id.im_13, R.id.im_14,
@@ -46,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         TextView welcomeTextView = findViewById(R.id.WelcomeMsg);
         welcomeTextView.setText("Welcome: " + username);
     }
+
 
     private void onNewGame() {
         shuffleCards();
@@ -85,6 +90,11 @@ public class MainActivity extends AppCompatActivity {
                 turnEnd(view);
             }
         }
+        // Update the last opened cards
+        if (lastOpenedCard1 != -1) {
+            lastOpenedCard2 = lastOpenedCard1;
+        }
+        lastOpenedCard1 = view.getId();
     }
 
     public void turnEnd(View view) {
@@ -132,12 +142,14 @@ public class MainActivity extends AppCompatActivity {
             ImageView cardClosed = findViewById(viewId[i]);
             cardClosed.setImageResource(R.drawable.card_back);
         }
+        lastOpenedCard1 = -1;
+        lastOpenedCard2 = -1;
     }
 
     public void closeCard(View view) {
-        for (int i = 0; i < viewId.length; i++) {
-            ImageView cardClosed = findViewById(viewId[i]);
-            cardClosed.setImageResource(R.drawable.card_back);
-        }
+        ImageView cardClosed1 = findViewById(lastOpenedCard1);
+        cardClosed1.setImageResource(R.drawable.card_back);
+        ImageView cardClosed2 = findViewById(lastOpenedCard2);
+        cardClosed2.setImageResource(R.drawable.card_back);
     }
 }
